d7e58e9486f05e64a925da4b4a226bdd97a37cdd
Apurv Pandey <apurv135068@nitp.ac.in>
2016-12-30 15:56:57 +0530

    Update MainActivity.java
    Comments added. Especially for consulting the layouts for display for publisher's and subscriber's stream.

--- Desktop/WebStreamingApplication/app/src/main/java/christm4s/webstreamingapplication/MainActivity.java
+++ Desktop/WebStreamingApplication/app/src/main/java/christm4s/webstreamingapplication/MainActivity.java
@@ -40,8 +40,10 @@ public class MainActivity extends AppCompatActivity implements Session.SessionLi
         Log.i(LOGTAG, "call to onCreate");
         super.onCreate(savedInstanceState);
 
+            
         LinearLayout parentLayout = new LinearLayout(this);
         setContentView(parentLayout);
+                    
 
         subscriberView = new LinearLayout(this);
         subscriberParams = new LinearLayout.LayoutParams
@@ -49,19 +51,23 @@ public class MainActivity extends AppCompatActivity implements Session.SessionLi
         subscriberParams.weight = 0.5f;
         subscriberView.setLayoutParams(subscriberParams);
 
+            
         publisherView = new LinearLayout(this);
         publisherParams = new LinearLayout.LayoutParams
                 (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         publisherParams.weight = 0.5f;
         publisherView.setLayoutParams(publisherParams);
 
+            
         parentLayout.setWeightSum(1f);
         parentLayout.addView(publisherView);
         parentLayout.addView(subscriberView);
 
+            
         Session session = new Session(MainActivity.this, API_KEY, SESSION_ID);
         session.setSessionListener(this);
         session.connect(TOKEN);
+    
     }
 
 
